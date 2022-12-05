b9a07294be601d002cdc9118fb19f891a154e4bc
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-12-05 18:03:07 +0100

    added template

---
+++ 2022/_day_template/src/bin/part_1.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let result = function(input);
+
+    println!("{}", result);
+}
+
+fn function(input: String) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_function() {
+        // read test input file into a string
+        let input = fs::read_to_string("test_input.txt").unwrap();
+
+        let result = function(input);
+
+        assert_eq!(result, 0);
+    }
+}
