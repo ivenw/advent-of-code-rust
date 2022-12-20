1f1bb64414d792fe7665a28d6144aa1fbe31b4d2
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-12-20 11:54:27 +0100

    finished part 2

--- 2022/day-08/src/bin/part_2.rs
+++ 2022/day-08/src/bin/part_2.rs
@@ -1,15 +1,12 @@
-use std::{fs, iter::zip};
+use std::fs;
 
 extern crate day_08;
 
 use day_08::input_parser::parse_input;
-use ndarray::{
-    iter::{Lanes, LanesMut},
-    Array2,
-};
+use ndarray::{s, Array2};
 
 fn main() {
-    let input = fs::read_to_string("test_input.txt").unwrap();
+    let input = fs::read_to_string("input.txt").unwrap();
 
     let result = function(input);
 
@@ -19,6 +16,33 @@ fn main() {
 fn function(input: String) -> u32 {
     let trees = parse_input(&input);
     let mut scenic_scores = Array2::from_elem(trees.dim(), 0);
+
+    for (idx, tree) in trees.indexed_iter() {
+        let mut scores = [0, 0, 0, 0];
+
+        let row = trees.row(idx.0);
+        let column = trees.column(idx.1);
+
+        let left = row.slice(s![..idx.1; -1]);
+        let right = row.slice(s![(idx.1 + 1)..]);
+        let up = column.slice(s![..idx.0; -1]);
+        let down = column.slice(s![(idx.0 + 1)..]);
+
+        for (i, direction) in [left, right, up, down].iter().enumerate() {
+            if direction.is_empty() {
+                continue;
+            }
+
+            for tree_ahead in direction {
+                scores[i] += 1;
+                if *tree_ahead >= *tree {
+                    break;
+                }
+            }
+        }
+
+        scenic_scores[idx] = scores.iter().product();
+    }
 
     scenic_scores.iter().max().unwrap().to_owned()
 }
